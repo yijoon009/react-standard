bb429cd1d71879184aa22cf4aa57999066d68dbd
yijoon009 <yijoon009@gmail.com>
2023-02-28 17:30:16 +0900

    1일차

---
+++ craco.config.js
@@ -0,0 +1,13 @@
+/**
+ * @description [Override] : 웹팩 설정
+ * @tutorial https://www.npmjs.com/package/@craco/craco
+ */
+
+const path = require("path");
+module.exports = {
+    webpack: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+};
