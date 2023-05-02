19c6c177d311432c44dda2333b4b0439484cb025
yijoon009 <yijoon009@gmail.com>
2023-05-02 18:00:00 +0900

    10일차

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,37 +1,179 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import styled from "styled-components";
+
 import { Table, TableBody, TableHead } from "@/style/Board";
-import { useState, useEffect } from "react";
-import { getSampleData } from "@/api/board";
-import UseAxiosInterceptor from "@/hook/useAxiosInterceptor";
+import { getSampleData2, Params } from "@/api/board";
+
+const CheckboxLabel = styled.label`
+    display: inline-block;
+    margin-right: 10px;
+    padding: 5px;
+`;
+
+type CheckboxItem = {
+    label: string;
+    checked: boolean;
+};
+
+type OptionType = {
+    value: string;
+    label: string;
+};
+
+type SearchCondition = {
+    evalYearSeq: number;
+    currentPageNo: number;
+    allLv: CheckboxItem[];
+    scaleLv: CheckboxItem[];
+    searchType: string;
+    searchWord: string;
+};
+
+const options: OptionType[] = [
+    { value: "unvrsNm", label: "종목명" },
+    { value: "asymbol", label: "종목코드" },
+];
+
+const checkList1: CheckboxItem[] = [
+    { label: "A", checked: false },
+    { label: "AA", checked: false },
+    { label: "B", checked: false },
+    { label: "BB", checked: false },
+];
+
+const checkList2: CheckboxItem[] = [
+    { label: "A", checked: false },
+    { label: "AA", checked: false },
+    { label: "B", checked: false },
+    { label: "BB", checked: false },
+];
+
 /**
  * @description 게시판
  */
 function Board() {
-    // useAxiosInterceptor 훅 적용
-    UseAxiosInterceptor();
-
     // state
+    const [searchCondition, setSearchCondition] = useState<SearchCondition>({
+        evalYearSeq: 1,
+        currentPageNo: 1,
+        allLv: checkList1,
+        scaleLv: checkList2,
+        searchType: "",
+        searchWord: "",
+    });
     const [list, setList] = useState<any[]>([]);
 
-    // method -> useEffect가 자체적인 비동기통신이 불가능하여 만들어준 메서드다.
-    const fetchRequest = async () => {
-        const result = await getSampleData();
+    // read-only
+    const { allLv, scaleLv, searchType, searchWord } = searchCondition;
 
-        const { resultList = [] } = result || {};
-
-        setList(resultList);
+    // event
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearchCondition((prevState) => {
+            return { ...prevState, searchWord: e.target.value };
+        });
     };
 
-    // watch
-    useEffect(() => {
-        // 최초 화면 렌더링 후 한번만 실행되는 영역
-        fetchRequest();
-    }, []);
+    const onSelect = (e: ChangeEvent<HTMLSelectElement>) => {
+        setSearchCondition((prevState) => {
+            return { ...prevState, searchType: e.target.value };
+        });
+    };
+
+    const onCheck = (
+        e: ChangeEvent<HTMLInputElement>,
+        index: number,
+        checkList: CheckboxItem[]
+    ) => {
+        // console.log(e.target.name, index);
+
+        const { checked, name } = e.target;
+
+        const updatedCheckList = checkList.map((item, i) =>
+            i === index ? { ...item, checked: checked } : item
+        );
+
+        setSearchCondition((prevState) => {
+            // [] 대괄호 안에 적으면 변수로 인식해서 [name] 으로 작성함.
+            // name 안에 allLv, scaleLv 두개가 들어갈 수 있기 때문
+            return { ...prevState, [name]: updatedCheckList };
+        });
+    };
+
+    const onSubmit = (e: FormEvent) => {
+        // submit 하면 무조건 새로고침 되는데
+        // 아래 줄을 작성하면 새로고침 막을 수 있음
+        e.preventDefault();
+
+        // console.log(searchCondition);
+
+        const params = {
+            ...searchCondition,
+            allLv: allLv
+                .filter((item) => item.checked)
+                .map((item) => item.label)
+                .join(","),
+            scaleLv: scaleLv
+                .filter((item) => item.checked)
+                .map((item) => item.label)
+                .join(","),
+        };
+
+        requestSampleData(params);
+    };
+
+    // method
+    const requestSampleData = async (params: Params) => {
+        const response = await getSampleData2(params);
+        // console.log(result);
+        const { resultList } = response;
+        setList(resultList);
+    };
 
     // view
     return (
         <>
-            <input />
-            <button>검색</button>
+            <form onSubmit={onSubmit}>
+                <div>
+                    전체등급 :
+                    {allLv.map(({ checked, label }, index) => (
+                        <CheckboxLabel key={index}>
+                            <input
+                                name="allLv"
+                                type="checkbox"
+                                checked={checked}
+                                onChange={(e) => onCheck(e, index, allLv)}
+                            />
+                            {label}
+                        </CheckboxLabel>
+                    ))}
+                    <select value={searchType} onChange={onSelect}>
+                        <option value="" disabled>
+                            선택하세요
+                        </option>
+                        {options.map(({ value, label }, index) => (
+                            <option key={index.toString()} value={value}>
+                                {label}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <div>
+                    규모등급 :
+                    {scaleLv.map(({ checked, label }, index) => (
+                        <CheckboxLabel key={index}>
+                            <input
+                                name="scaleLv"
+                                type="checkbox"
+                                checked={checked}
+                                onChange={(e) => onCheck(e, index, scaleLv)}
+                            />
+                            {label}
+                        </CheckboxLabel>
+                    ))}
+                    <input value={searchWord} onChange={onChange} />
+                    <button>검색</button>
+                </div>
+            </form>
             <Table>
                 <colgroup>
                     <col />
@@ -54,21 +196,21 @@ function Board() {
                     {list.map(
                         (
                             {
-                                aflNm,
-                                assetScaleCcdNm,
+                                allLvReslut,
+                                assetScale,
                                 asymbol,
-                                currGrade,
-                                gicsLv2Nm,
+                                scaleLvResult,
+                                unvrsNm,
                             },
                             index
                         ) => (
                             <tr key={index}>
                                 <th scope="row">{index + 1}</th>
-                                <td>{aflNm}</td>
-                                <td>{assetScaleCcdNm}</td>
+                                <td>{allLvReslut}</td>
+                                <td>{assetScale}</td>
                                 <td>{asymbol}</td>
-                                <td>{currGrade}</td>
-                                <td>{gicsLv2Nm}</td>
+                                <td>{scaleLvResult}</td>
+                                <td>{unvrsNm}</td>
                             </tr>
                         )
                     )}
